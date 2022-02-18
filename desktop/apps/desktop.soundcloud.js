2d2507acb6d3f90c133cb8d752e4e007b278bf54
Marak <marak.squires@gmail.com>
2022-02-18 12:14:32 -0500

    Adds music playlist selector to top nav bar Can now specify which playlist loads in SoundCloud

--- desktop/apps/desktop.soundcloud.js
+++ desktop/apps/desktop.soundcloud.js
@@ -5,21 +5,27 @@ desktop.soundcloud.load = function loadSoundcloud () {
     desktop.soundcloud.widget.play();
     desktop.soundcloud.widget.next();
   });
+  $('.selectPlaylist').on('change', function(){
+    $('#soundcloudiframe').remove();
+    $('#soundcloudplayer').html('');
+    desktop.soundcloud.embeded = false;
+    $('#window_soundcloud').show();
+    desktop.soundcloud.openWindow($(this).val());
+  })
 }
 
 desktop.soundcloud.embeded = false;
 
-desktop.soundcloud.openWindow = function openWindow () {
+desktop.soundcloud.openWindow = function openWindow (playlistID) {
+  playlistID = playlistID || "1397493787"
   if (!desktop.soundcloud.embeded) {
     desktop.soundcloud.embeded = true;
-    // TODO: made actual iframe object
     let str = `
-      <iframe id="soundcloudiframe" width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1397230333&color=%23df23e4&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/marak" title="marak" target="_blank" style="color: #cccccc; text-decoration: none;">marak</a> · <a href="https://soundcloud.com/marak/sets/buddy-pond-0" title="Buddy Pond 0" target="_blank" style="color: #cccccc; text-decoration: none;">Buddy Pond 0</a></div>
+      <iframe id="soundcloudiframe" width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/${playlistID}&color=%23df23e4&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/marak" title="marak" target="_blank" style="color: #cccccc; text-decoration: none;">marak</a> · <a href="https://soundcloud.com/marak/sets/buddy-pond-0" title="Buddy Pond 0" target="_blank" style="color: #cccccc; text-decoration: none;">Buddy Pond 0</a></div>
       `;
     $('#soundcloudplayer').append(str);
     $('#soundcloudiframe').on("load", function() {
       var tag = document.createElement('script');
-      // Remark: Do we need to use onload event here for more reliable embed?
       tag.onload = function() {
         var iframeElement   = document.querySelector('#soundcloudiframe');
         var iframeElementID = iframeElement.id;
