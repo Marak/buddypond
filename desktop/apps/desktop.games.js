dbfa2b4ac800143e41cfc63035940fa42dc55c4e
Deividy Metheler Zachetti <deividyz@gmail.com>
2022-02-19 15:41:52 -0300

    add solitaire game and games folder

---
+++ desktop/apps/desktop.games.js
@@ -0,0 +1,10 @@
+desktop.games = {};
+
+desktop.games.load = function loadDesktopGames () {
+  $('#window_games').css('width', 686);
+  $('#window_games').css('height', 150);
+  $('#window_games').css('left', 200);
+  $('#window_games').css('top', 250);
+
+  return true;
+};
