21af0f714a6222be69938d718a9d4e7c361f1446
Marak <marak.squires@gmail.com>
2022-02-23 21:59:54 -0500

    Code clean-up

--- desktop/apps/desktop.localstorage/desktop.localstorage.js
+++ desktop/apps/desktop.localstorage/desktop.localstorage.js
@@ -18,13 +18,11 @@ desktop.localstorage.removeItem = function removeLocalStorage (key) {
   localStorage.removeItem(desktop.localstorage.prefix + key);
 }
 
-desktop.localstorage.load = function loadlocalstorageGames () {
+desktop.localstorage.load = function loadLocalStorage () {
   // boot all localstorage data into local settings
   for (var key in localStorage){
     if (key.search(desktop.localstorage.prefix) !== -1) {
       let param = key.replace(desktop.localstorage.prefix, '');
-      console.log('booting', param, localStorage[key])
-      
       try {
         desktop.settings[param] = JSON.parse(localStorage[key]);
       } catch (err) {
