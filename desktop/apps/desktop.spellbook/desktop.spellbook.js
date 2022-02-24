1732defb2fbeb1085d5b5bfb2184bc06e4fd800b
Marak <marak.squires@gmail.com>
2022-02-24 00:30:51 -0500

    Adds Zalgo spell. Raises desktop console limit to Infinity

--- desktop/apps/desktop.spellbook/desktop.spellbook.js
+++ desktop/apps/desktop.spellbook/desktop.spellbook.js
@@ -2,6 +2,7 @@ desktop.spellbook = {};
 desktop.spellbook.label = "Spellbook";
 desktop.spellbook.spells = [
   'ebublio',
+  'zalgo',
   'rickroll',
   'alert',
   'logout',
@@ -113,3 +114,144 @@ desktop.spellbook.ebublio = function lightingBolt () {
   });
 });
 }
+
+
+// please no
+desktop.spellbook.zalgo = function castZalgoSpell (params, next) {
+  $('span, label, a').each(function(i, item){
+    if($(this).css('display') === 'block') {
+        $(this).attr('data-og-zalgo', $(this).html());
+        $(this).html(desktop.spellbook._zalgo($(this).html()))
+        $(this).addClass('rainbowFast');
+      }
+  });
+  let count = 0;
+  let zalgoInt = setInterval(function(){
+    $('span, label, a').each(function(i, item){
+      if($(this).css('display') === 'block') {
+        $(this).html(desktop.spellbook._zalgo($(this).attr('data-og-zalgo')))
+      }
+    });
+    count++;
+    if (count > 13) {
+      clearInterval(zalgoInt);
+      $('span, label, a').each(function(i, item){
+        if ($(this).css('display') === 'block') {
+          $(this).html($(this).attr('data-og-zalgo'))
+          $(this).removeClass('rainbowFast');
+        }
+      });
+    }
+  }, 1666)
+} 
+
+// he comes
+let _zalgo = desktop.spellbook._zalgo = function zalgo(text, options) {
+  text = text || '   he is here   ';
+  var soul = {
+    'up': [
+      '̍', '̎', '̄', '̅',
+      '̿', '̑', '̆', '̐',
+      '͒', '͗', '͑', '̇',
+      '̈', '̊', '͂', '̓',
+      '̈', '͊', '͋', '͌',
+      '̃', '̂', '̌', '͐',
+      '̀', '́', '̋', '̏',
+      '̒', '̓', '̔', '̽',
+      '̉', 'ͣ', 'ͤ', 'ͥ',
+      'ͦ', 'ͧ', 'ͨ', 'ͩ',
+      'ͪ', 'ͫ', 'ͬ', 'ͭ',
+      'ͮ', 'ͯ', '̾', '͛',
+      '͆', '̚',
+    ],
+    'down': [
+      '̖', '̗', '̘', '̙',
+      '̜', '̝', '̞', '̟',
+      '̠', '̤', '̥', '̦',
+      '̩', '̪', '̫', '̬',
+      '̭', '̮', '̯', '̰',
+      '̱', '̲', '̳', '̹',
+      '̺', '̻', '̼', 'ͅ',
+      '͇', '͈', '͉', '͍',
+      '͎', '͓', '͔', '͕',
+      '͖', '͙', '͚', '̣',
+    ],
+    'mid': [
+      '̕', '̛', '̀', '́',
+      '͘', '̡', '̢', '̧',
+      '̨', '̴', '̵', '̶',
+      '͜', '͝', '͞',
+      '͟', '͠', '͢', '̸',
+      '̷', '͡', ' ҉',
+    ],
+  };
+  var all = [].concat(soul.up, soul.down, soul.mid);
+
+  function randomNumber(range) {
+    var r = Math.floor(Math.random() * range);
+    return r;
+  }
+
+  function isChar(character) {
+    var bool = false;
+    all.filter(function(i) {
+      bool = (i === character);
+    });
+    return bool;
+  }
+
+
+  function heComes(text, options) {
+    var result = '';
+    var counts;
+    var l;
+    options = options || {};
+    options['up'] =
+      typeof options['up'] !== 'undefined' ? options['up'] : true;
+    options['mid'] =
+      typeof options['mid'] !== 'undefined' ? options['mid'] : true;
+    options['down'] =
+      typeof options['down'] !== 'undefined' ? options['down'] : true;
+    options['size'] =
+      typeof options['size'] !== 'undefined' ? options['size'] : 'maxi';
+    text = text.split('');
+    for (l in text) {
+      if (isChar(l)) {
+        continue;
+      }
+      result = result + text[l];
+      counts = {'up': 0, 'down': 0, 'mid': 0};
+      switch (options.size) {
+        case 'mini':
+          counts.up = randomNumber(4);
+          counts.mid = randomNumber(2);
+          counts.down = randomNumber(4);
+          break;
+        case 'maxi':
+          counts.up = randomNumber(4) + 3;
+          counts.mid = randomNumber(4) + 1;
+          counts.down = randomNumber(16) + 3;
+          break;
+        default:
+          counts.up = randomNumber(8) + 1;
+          counts.mid = randomNumber(6) / 2;
+          counts.down = randomNumber(8) + 1;
+          break;
+      }
+
+      var arr = ['up', 'mid', 'down'];
+      for (var d in arr) {
+        var index = arr[d];
+        for (var i = 0; i <= counts[index]; i++) {
+          if (options[index]) {
+            result = result + soul[index][randomNumber(soul[index].length)];
+          }
+        }
+      }
+    }
+    return result;
+  }
+  // don't summon him
+  return heComes(text, options);
+};
+
