46e972b51d917aa0c92c64e7a35f563cc3e877e5
Marak <marak.squires@gmail.com>
2024-12-28 13:34:59 -1000

    Adds `isTyping` logic to buddylist

--- v2/apps/based/buddylist/buddylist.js
+++ v2/apps/based/buddylist/buddylist.js
@@ -1,6 +1,3 @@
-//import uuid from 'https://cdn.jsdelivr.net/npm/uuid@11.0.3/+esm'
-
-
 import processBuddylist from "./lib/processProfile.js";
 import renderOrUpdateBuddyInBuddyList from "./lib/renderOrUpdateBuddyInBuddyList.js";
 import renderChatMessage from "./lib/renderChatMessage.js";
@@ -8,7 +5,9 @@ import renderBuddyRequests from "./lib/renderBuddyRequests.js";
 import buddylistUIEvents from "./lib/buddylistUIEvents.js";
 import openChatWindow from "./lib/openChatWindow.js";
 
-// import { v4 as uuid } from 'uuid';
+// TODO: import { v4 as uuid } from 'uuid';
+// TODO: why does client care about making UUID at all?
+// this should be responsibility of the server
 function uuid() {
     return new Date().getTime();
 }
@@ -26,9 +25,11 @@ export default class BuddyList {
         this.subscribedBuddies = [];
         this.subscribedPonds = [];
         this.options = options;
+        this.opened = false;
     }
 
     async init() {
+
     }
 
     async open(config = { type: 'buddylist-profile' }) {
@@ -37,11 +38,22 @@ export default class BuddyList {
         // 'buddylist-profile' - the default buddylist window
         // 'buddylist-chat' - a chat window
 
+      
         if (typeof config.type !== 'string') {
             config.type = 'buddylist-profile';
         }
 
         if (config.type === 'buddylist-profile') {
+
+
+            if (this.opened) {
+                this.buddyListWindow.open();
+                this.bp.apps.ui.windowManager.focusWindow(this.buddyListWindow);
+                return 'buddylist already open';
+            }
+
+            this.opened = true;
+
             const htmlStr = await this.bp.fetchHTMLFragment('/v2/apps/based/buddylist/buddylist.html');
             this.messageTemplateString = await this.bp.fetchHTMLFragment('/v2/apps/based/buddylist/message.html');
             this.bp.appendCSS('/v2/apps/based/buddylist/buddylist.css');
@@ -53,15 +65,18 @@ export default class BuddyList {
 
             const buddyListWindow = this.createBuddyListWindow();
             buddyListWindow.content.appendChild(this.createHTMLContent(htmlStr));
-
+            this.buddyListWindow = buddyListWindow;
             this.registerEventHandlers();
             this.handleAuthentication();
             this.buddylistUIEvents();
 
             $('.loggedIn').hide();
+
             return 'hello buddyList';
         }
 
+        // Remark: is this code still used? can we remove? handled by openChatWindow
+        // called from elsewhere?
         if (config.type === 'pond') {
             // the type of window is a chat window
             // we *don't* need to re-render the buddylist-profile 
@@ -102,7 +117,112 @@ export default class BuddyList {
         this.bp.on('profile::buddy::newmessage', 'open-chat-window', data => this.openChatWindow(data));
         this.bp.on('profile::buddy::newmessage', 'mark-messages-as-read', data => this.buddyReadNewMessages(data));
 
+        this.bp.on('profile::buddy::calling', 'start-call', data => {
+            // legacy BP API
+            console.log("profile::buddy::calling", data);
+            desktop.app.videochat.startCall(false, data.name, function (err, re) {
+                console.log('startCall callback', err, re);
+            });
+
+        });
+
+
+
         this.bp.on('profile::status', 'update-profile-status', status => status === 'signout' && this.logout());
+
+        this.bp.on('buddy::messages', 'render-chat-message', data => this.handleChatMessages(data));
+        this.bp.on('buddy::sendMessage', 'send-buddy-message-to-server', data => this.sendMessageToServer(data));
+        this.bp.on('pond::sendMessage', 'send-pond-message-to-server', data => this.sendPondMessageToServer(data));
+
+        this.bp.on('buddy::sendMessage', 'process-buddymessage-bs', data => this.bp.apps.buddyscript.parseCommand(data.text));
+        this.bp.on('pond::sendMessage', 'process-pondmessage-bs', data => this.bp.apps.buddyscript.parseCommand(data.text));
+
+
+        // remote isTyping event from server
+        this.bp.on("buddy::isTyping", "show-is-typing-message", data => {
+
+            console.log("isTyping", data);
+            // TODO: move to separate file
+            let message = data.message;
+
+            // TODO: move this to a separate file / function
+            // Handling typing message display
+            if (message.isTyping === true) {
+                console.log('incoming isTyping message', message);
+                // check to see if message.from is the same as the current user
+                // if so, ignore the message
+                if (message.from === this.bp.me) {
+                    return;
+                }
+
+                // check the ctime of the message
+                // console.log("isTyping message", message);
+                let messageTime = new Date(message.ctime);
+                // console.log("messageTime", messageTime.getTime());
+                let now = new Date().getTime();
+                let selector = `#${message.type}_message_-${message.from}`;
+                // alert(selector);
+                let chatWindow = $(selector);
+                // don't process isTyping messages over 3 seconds old
+                if (now - messageTime.getTime() > 3000) {
+                    // console.log("isTyping message too old", message);
+                    return;
+                }
+
+                let typingIndicatorId = `typing-${message.from}`;
+                let typingMessage = `${message.from} is typing...`;
+
+                // Check if the typing indicator for this user already exists
+                let typingIndicator = $(`.aim-typing span[data-user="${message.from}"]`, chatWindow.content);
+                if (typingIndicator.length === 0) {
+                    // If it does not exist, create a new span and append it to the .aim-typing area
+                    typingIndicator = $('<span></span>')
+                        .attr('data-user', message.from)
+                        .text(typingMessage)
+                        .appendTo($('.aim-typing', chatWindow.content));
+                } else {
+                    // If it exists, just update the text
+                    typingIndicator.text(typingMessage);
+                }
+
+                // Clear any existing timeout for this user
+                // console.log("CLEARING OLD TIMER")
+                if (this.showingIsTyping[typingIndicatorId]) {
+                    clearTimeout(this.showingIsTyping[typingIndicatorId]);
+                }
+
+                // console.log("CREATING NEW TIMER")
+                // Set a new timeout to remove the typing message after 3 seconds
+                this.showingIsTyping[typingIndicatorId] = setTimeout(() => {
+                    typingIndicator.remove();
+                }, 1500);
+                return;
+            }
+        })
+
+        // local typing event TOOD: better name
+        // when buddy is typing send a message to the ws server
+        this.bp.on('buddy::typing', 'send-typing-message-to-server', data => {
+            console.log("typing", data);
+            // we don't want to spam typing messages, so we will only send a message every 2 seconds
+            this.lastTypingMessage = this.lastTypingMessage || 0;
+            if (new Date().getTime() - this.lastTypingMessage < 2000) {
+                // return;
+            }
+            console.log("send typing message", data);
+            this.lastTypingMessage = new Date().getTime();
+
+
+            if (data.type === 'pond') {
+                this.sendPondMessageToServer(data);
+            } else {
+                this.sendMessageToServer(data);
+            }
+            // this.bp.apps.client.sendMessage({ id: uuid(), method: 'sendMessage', data: data });
+
+
+        });
+
         // TODO: this handler could instead bind to bp.apps.system.messages
         // a System allows for sending and receiving messages to a sequence of handlers
         /*
@@ -156,17 +276,6 @@ export default class BuddyList {
         });
         */
 
-
-
-
-        this.bp.on('buddy::messages', 'render-chat-message', data => this.handleChatMessages(data));
-        this.bp.on('buddy::sendMessage', 'send-buddy-message-to-server', data => this.sendMessageToServer(data));
-        this.bp.on('pond::sendMessage', 'send-pond-message-to-server', data => this.sendPondMessageToServer(data));
-
-        this.bp.on('buddy::sendMessage', 'process-buddymessage-bs', data => this.bp.apps.buddyscript.parseCommand(data.text));
-        this.bp.on('pond::sendMessage', 'process-pondmessage-bs', data => this.bp.apps.buddyscript.parseCommand(data.text));
-
-
     }
 
     createHTMLContent(htmlStr) {
@@ -199,7 +308,7 @@ export default class BuddyList {
             await this.renderChatMessage(message);
         }
     }
-    
+
     sendMessageToServer(data) {
         this.bp.log('buddy::sendMessage', data);
         data.uuid = uuid();
@@ -216,7 +325,7 @@ export default class BuddyList {
         this.bp.log('pond::sendMessage', data);
         data.uuid = uuid();
         data.pondname = data.to;
-
+        // console.log('sendPondMessageToServer', data);
         this.bp.apps.client.sendMessage({ id: data.uuid, method: 'sendMessage', data: data });
         const chatWindow = this.openChatWindow(data);
         this.renderChatMessage(data, chatWindow);
@@ -263,16 +372,15 @@ BuddyList.prototype.renderBuddyRequests = renderBuddyRequests;
 BuddyList.prototype.processBuddylist = processBuddylist;
 BuddyList.prototype.buddylistUIEvents = buddylistUIEvents;
 BuddyList.prototype.openChatWindow = openChatWindow;
+
 BuddyList.prototype.logout = function () {
-
     // close any open chat windows
-    $('.chatWindow').remove();
-
+    $('.chatWindow').remove(); // maybe, they could stay open as well
+    // disconnect the client
     this.bp.apps.client.disconnect();
     $('.password').val('');
     $('.loggedIn').hide();
     $('.loggedOut').show();
     localStorage.removeItem('qtokenid');
     localStorage.removeItem('me');
-    // disconnect the client
 }
