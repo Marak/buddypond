a95d166b0bc40e455970a12b956bbe5dbf51f7e2
Marak <marak.squires@gmail.com>
2024-12-31 19:35:26 -1000

    Use correct host path for client

--- v5/apps/based/client/client.js
+++ v5/apps/based/client/client.js
@@ -11,7 +11,7 @@ export default class Client {
         };
         this.ws = null;
         this.api = api;
-        this.api.endpoint = this.config.host + '/api/v3';
+        this.api.endpoint = this.config.host;
         this.connectionSources = {};  // Tracks WebSocket connection requests by source
         this.disconnectTimer = null;
         this.disconnectDelay = 10000;  // 10 seconds
