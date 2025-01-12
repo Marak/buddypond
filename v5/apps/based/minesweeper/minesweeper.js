18a623c18aa8edd26bf7e5a8f64fe7370dde16e7
Marak <marak.squires@gmail.com>
2025-01-11 16:35:06 -1000

    Updates `solitaire` and `minesweeper` to v5 API

---
+++ v5/apps/based/minesweeper/minesweeper.js
@@ -0,0 +1,55 @@
+export default class Minesweeper {
+    constructor(bp, options = {}) {
+        this.bp = bp;
+        return this;
+    }
+
+    async init() {
+        this.bp.log('Hello from Minesweeper');
+
+        // we can load modules or html fragments or css files here
+        // using this.bp.load() method
+
+        // injects CSS link tag into the head of document
+        // await this.bp.load('/v5/apps/based/_example/_example.css');
+
+        // fetches html from the fragment and returns it as a string
+        // let html = await this.bp.load('/v5/apps/based/_example/_example.html');
+
+        // await imports the module and returns it
+        // let module = await this.bp.load('/v5/apps/based/_example/_example.js');
+
+        return 'loaded Minesweeper';
+    }
+
+    async open(options = {}) {
+        if (!this.minesweeperWindow) {
+            this.minesweeperWindow = this.bp.apps.ui.windowManager.createWindow({
+                id: 'minesweeper',
+                title: 'Minesweeper',
+                x: 50,
+                y: 100,
+                width: 600,
+                height: 300,
+                minWidth: 200,
+                minHeight: 200,
+                parent: $('#desktop')[0],
+                iframeContent: '/v5/apps/based/minesweeper/vendor/minesweeper.html',
+                resizable: true,
+                minimizable: true,
+                maximizable: true,
+                closable: true,
+                focusable: true,
+                maximized: false,
+                minimized: false,
+                onClose: () => {
+                    this.minesweeperWindow = null;
+                }
+            });
+
+        }
+
+        this.bp.apps.ui.windowManager.focusWindow(this.minesweeperWindow);
+
+    }
+}
---
+++ v5/apps/based/solitaire/solitaire.js
@@ -0,0 +1,43 @@
+export default class Solitaire {
+    constructor(bp, options = {}) {
+        this.bp = bp;
+        return this;
+    }
+
+    async init() {
+        this.bp.log('Hello from Solitaire');
+
+        return 'loaded Solitaire';
+    }
+
+    async open(options = {}) {
+        if (!this.solitaireWindow) {
+            this.solitaireWindow = this.bp.apps.ui.windowManager.createWindow({
+                id: 'solitaire',
+                title: 'Solitaire',
+                x: 50,
+                y: 100,
+                width: 600,
+                height: 300,
+                minWidth: 200,
+                minHeight: 200,
+                parent: $('#desktop')[0],
+                iframeContent: '/v5/apps/based/solitaire/vendor/solitaire.html',
+                resizable: true,
+                minimizable: true,
+                maximizable: true,
+                closable: true,
+                focusable: true,
+                maximized: false,
+                minimized: false,
+                onClose: () => {
+                    this.pianoWindow = null;
+                }
+            });
+
+        }
+
+        this.bp.apps.ui.windowManager.focusWindow(this.solitaireWindow);
+
+    }
+}
