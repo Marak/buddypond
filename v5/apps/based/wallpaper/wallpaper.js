b15bb32c317e800ee0470f972f2628e7b147d8a9
Marak <marak.squires@gmail.com>
2025-01-12 12:33:00 -1000

    Removes un-used import

--- v5/apps/based/wallpaper/wallpaper.js
+++ v5/apps/based/wallpaper/wallpaper.js
@@ -8,20 +8,7 @@ export default class Wallpaper {
     }
 
     async init() {
-        this.bp.log('Hello from Example');
-
-        // we can load modules or html fragments or css files here
-        // using this.bp.load() method
-
-        // injects CSS link tag into the head of document
-        await this.bp.load('/v5/apps/based/_example/_example.css');
-
-        // fetches html from the fragment and returns it as a string
-        let html = await this.bp.load('/v5/apps/based/_example/_example.html');
-
-        // await imports the module and returns it
-        //        let module = await this.bp.load('/v5/apps/based/_example/_example.js');
-
+        this.bp.log('Hello from Wallpaper');
 
 
         // Usage:
