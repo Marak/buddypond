d365138e2c75df6de042400636b561b250e1e0c1
Marak <marak.squires@gmail.com>
2025-02-04 10:26:50 -1000

    Adds initial version of `coin`

--- v5/apps/based/coin/coin.js
+++ v5/apps/based/coin/coin.js
@@ -1,18 +1,42 @@
-/* coins.js - Marak Squires 2025 - BuddyPond */
+/* Coin.js - Marak Squires 2025 - BuddyPond */
 export default class Coin {
-    constructor(bp, settings = {}) {
+
+    constructor(bp, options = {}) {
         this.bp = bp;
-        this.apiEndPoint = 'https://a.buddypond.com/api/v6/coins';
-        this.apiEndPoint = 'https://192.168.200.59/api/v6/coins';
+        return this;
     }
 
-    init() {
-
+    async init() {
+        this.html = await this.bp.load('/v5/apps/based/coin/coin.html');
+        this.css = await this.bp.load('/v5/apps/based/coin/coin.css');
     }
 
-    open () {
-        // TODO: leave this stub here for now
-        console.log('Stub for opening UI window');
-    }
-
+    async open () {
+        if (!this.coinWindow) {
+            this.coinWindow = this.bp.apps.ui.windowManager.createWindow({
+                id: 'coin',
+                title: 'Coin',
+                icon: 'desktop/assets/images/icons/icon_console_64.png',
+                x: 250,
+                y: 75,
+                width: 800,
+                height: 600,
+                minWidth: 200,
+                minHeight: 200,
+                parent: $('#desktop')[0],
+                content: this.html,
+                resizable: true,
+                minimizable: true,
+                maximizable: true,
+                closable: true,
+                focusable: true,
+                maximized: false,
+                minimized: false,
+                onClose: () => {
+                   this.coinWindow = null;
+                }
+            });
+        }
+    }  
+    
 }
