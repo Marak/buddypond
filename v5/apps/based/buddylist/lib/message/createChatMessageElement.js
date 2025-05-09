65eaca1d68fd6699e4efb22e62569fae0f7908c6
Marak <marak.squires@gmail.com>
2025-05-09 10:21:36 -1000

    Refactors chat message element creation to separate functions

--- v5/apps/based/buddylist/lib/message/createChatMessageElement.js
+++ v5/apps/based/buddylist/lib/message/createChatMessageElement.js
@@ -1,11 +1,18 @@
 import renderGeoFlag from './renderGeoFlag.js';
 import parseMarkdownWithoutPTags from './parseMarkdownWithoutPTags.js';
 
-// TODO: make useMarkdown a user setting
 let useMarkdown = true;
-// TODO: make allowHTML a user setting
 let allowHTML = true;
 
+// TODO: prefix all class names with 'aim-' to avoid conflicts
+// TODO: css styles will be in a separate file
+// TODO: make messages look and feel more like the Discord chat
+// TODO: add personal profile picture as leftmost element
+// TODO: move geoflag to under the personal profile picture
+// TODO: add space for badges in the same area as the geoflag
+// TODO: move name and timestamp to top of the message
+// TODO: create mouse-over on the message that shows hover menu on top right of the message
+//       mouse over tooltip should have: Add Reaction, Edit Message, Reply Message, and a "..." option for creating a context menu ( do not implement context menu here / yet )
 
 export default function createChatMessageElement(message, messageTime, chatWindow, container) {
     // Create elements
@@ -20,16 +27,15 @@ export default function createChatMessageElement(message, messageTime, chatWindo
     // Determine message sender and apply styles
     let senderText;
     if (message.from === 'anonymous') {
-      // Include a tripcode for anonymous messages; use a default if none provided
-      let tripcode = message.tripcode || 'tr1pc0d3';
-      senderText = `${message.from} (${tripcode}): `;
+        // Include a tripcode for anonymous messages; use a default if none provided
+        let tripcode = message.tripcode || 'tr1pc0d3';
+        senderText = `${message.from} (${tripcode}): `;
     } else {
-      // For non-anonymous users, just use the username followed by a colon
-      senderText = `${message.from}: `;
+        // For non-anonymous users, just use the username followed by a colon
+        senderText = `${message.from}: `;
     }
 
     let messageSender = document.createElement('span');
-    // TODO: remove from messageSender, use parent element
     messageSender.setAttribute('data-from', message.from);
     messageSender.setAttribute('data-to', message.to);
     messageSender.setAttribute('data-type', message.type);
@@ -45,12 +51,10 @@ export default function createChatMessageElement(message, messageTime, chatWindo
     // Combine elements for the sender
     messageSender.prepend(geoFlag);  // Prepend geoFlag to the sender span
 
-
     // render as markdown ( if enabled )
-    // TODO: should probably be a markdown registered as messagesProcessor with SystemsManager
     if (useMarkdown) {
-      message.text = parseMarkdownWithoutPTags(message.text);
-      // console.log('message.text', message.text)
+        message.text = parseMarkdownWithoutPTags(message.text);
+        // console.log('message.text', message.text)
     }
 
     // Prepare the message content span
@@ -58,8 +62,8 @@ export default function createChatMessageElement(message, messageTime, chatWindo
     messageContent.className = `message ${messageClass}`;
 
     if (allowHTML) {
-      $(messageContent).html(message.text);
-      //messageContent.textContent = message.text; // Assuming 'message.text' contains the message text
+        $(messageContent).html(message.text);
+        //messageContent.textContent = message.text; // Assuming 'message.text' contains the message text
 
     }
 
@@ -69,10 +73,8 @@ export default function createChatMessageElement(message, messageTime, chatWindo
     chatMessage.appendChild(messageContent);
     chatMessage.appendChild(document.createElement('br'));
 
-    const aimMessages = chatWindow.content.querySelector('.aim-messages');
-
     if (container) {
-      chatMessage.appendChild(container);
+        chatMessage.appendChild(container);
     } else {
 
     }
@@ -86,29 +88,35 @@ export default function createChatMessageElement(message, messageTime, chatWindo
     // After each image loads attempt to scroll to the bottom
     // Without this code, the scroll to bottom functionality will not scroll all the way down
     $(chatMessage).find('img').on('load', function () {
-      // Scroll again after each image loads
-      // TODO: add back
-      // scrollToBottom();
+        // Scroll again after each image loads
+        // TODO: add back
+        // scrollToBottom();
     });
 
+    insertChatMessage(chatWindow, message, chatMessage);
+
+}
+
+function insertChatMessage(chatWindow, message, chatMessage) {
+
     // Find all messages in chat ordered by data-id
+    const aimMessages = chatWindow.content.querySelector('.aim-messages');
     let allMessages = Array.from(aimMessages.querySelectorAll('.chatMessage'));
     let inserted = false;
 
     // Iterate over existing messages to find the correct insertion point
     for (let existingMessage of allMessages) {
-      let existingId = parseInt(existingMessage.getAttribute('data-id'), 10);
-      if (message.id < existingId) {
-        aimMessages.insertBefore(chatMessage, existingMessage); // Insert before the first larger ID
-        inserted = true;
-        break;
-      }
+        let existingId = parseInt(existingMessage.getAttribute('data-id'), 10);
+        if (message.id < existingId) {
+            aimMessages.insertBefore(chatMessage, existingMessage); // Insert before the first larger ID
+            inserted = true;
+            break;
+        }
     }
 
     // If no larger ID was found, append it to the end
     // TODO: should return the chatMessage element instead of appending it
     if (!inserted) {
-      aimMessages.appendChild(chatMessage);
+        aimMessages.appendChild(chatMessage);
     }
-
-  }
+}
