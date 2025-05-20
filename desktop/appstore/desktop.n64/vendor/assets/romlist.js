3e6cc01f2cf28e1630f21b0aef4c1b97b1038198
Marak <marak.squires@gmail.com>
2025-05-19 23:54:16 -1000

    Removing legacy apps

--- desktop/appstore/desktop.n64/vendor/assets/romlist.js
+++
@@ -1,7 +0,0 @@
-var ROMLIST = [
-    /*
-    {url:"roms/rom1.z64",title:"Game 1"},
-    {url:"roms/rom2.v64",title:"Game 2"},
-    {url:"roms/rom3.v64",title:"Game 3"},
-    */
-];
--- desktop/appstore/desktop.visuals/vendor/README.md
+++
@@ -1,2 +0,0 @@
-# lachlan-butterchurn
-Butterchurn music visualizer implementation with my own custom logo
