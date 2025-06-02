f310dcfc7cc1d5ec4deb2da02d3d1b598930c1f7
Marak <marak.squires@gmail.com>
2025-06-02 09:38:27 -1000

    Adds number formatting for coin rewards

--- v5/apps/based/rewards/rewards.js
+++ v5/apps/based/rewards/rewards.js
@@ -1,50 +1,48 @@
 export default class Rewards {
-    constructor(bp, options = {}) {
-        this.bp = bp;
-        this.options = options;
-        this.rewardInterval = null; // Placeholder for reward interval
-        return this;
+  constructor(bp, options = {}) {
+    this.bp = bp;
+    this.options = options;
+    this.rewardInterval = null; // Placeholder for reward interval
+    return this;
+  }
+
+  async init() {
+    //this.html = await this.bp.load('/v5/apps/based/motd/motd.html');
+    //await this.bp.load('/v5/apps/based/motd/motd.css');
+    return 'loaded rewards';
+  }
+
+  async requestReward() {
+    if (this.bp.apps.buddylist.client) {
+      // console.log('requesting rewards');
+      // TODO: make bp.apps.buddylist.client.wsClient.send into a method
+      // add a mutex to avoid spamming the request
+      this.bp.apps.buddylist.client.wsClient.send(JSON.stringify({
+        action: 'rewards:request',
+        buddyname: this.bp.me,
+        qtokenid: this.bp.qtokenid,
+      }));
     }
+  };
 
-    async init() {
-        //this.html = await this.bp.load('/v5/apps/based/motd/motd.html');
-        //await this.bp.load('/v5/apps/based/motd/motd.css');
-
-        // create a 60 second interval to send wsClient message with action 'rewards:request'
-        this.rewardInterval = setInterval(() => {
-
-            // this.requestReward();
-
-        }, 5 * 1000); // 60 seconds
-        // this.requestReward();
-        return 'loaded rewards';
-    }
-
-    async requestReward() {
-
-        if (this.bp.apps.buddylist.client) {
-            console.log('requesting rewards');
-            this.bp.apps.buddylist.client.wsClient.send(JSON.stringify({
-                action: 'rewards:request',
-                buddyname: this.bp.me,
-                qtokenid: this.bp.qtokenid,
-            }));
-        }
-
-
-    };
-
-    async open() { }
+  async open() { }
 
 }
-
 window.rollToNumber = function rollToNumber($el, value) {
-  const digits = String(value).split('');
+  // Format number with commas
+  const formattedValue = value.toLocaleString('en-US');
+  const digits = formattedValue.split('');
 
   // Clear and rebuild digits
   $el.empty();
 
-  digits.forEach(d => {
+  digits.forEach((d, index) => {
+    // Handle comma separately
+    if (d === ',') {
+      $el.append('<span class="odometer-comma">,</span>');
+      return;
+    }
+
     const digitContainer = $('<div class="odometer-digit"></div>');
     const inner = $('<div class="odometer-digit-inner"></div>');
 
@@ -55,9 +53,12 @@ window.rollToNumber = function rollToNumber($el, value) {
     digitContainer.append(inner);
     $el.append(digitContainer);
 
-    // Delay added to force DOM layout flush
+    // Delay added to force DOM layout flush, staggered for each digit
     setTimeout(() => {
-      inner.css('transform', `translateY(-${d * 1}em)`);
-    }, 10);
+      inner.css({
+        'transition': 'transform 0.5s ease-in-out', // Smooth transition
+        'transform': `translateY(-${d * 1}em)`
+      });
+    }, 50 + index * 100); // Base delay + staggered delay per digit
   });
 }
