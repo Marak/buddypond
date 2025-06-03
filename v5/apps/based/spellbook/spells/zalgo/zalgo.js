9be65dbf5f2a3f4926ba319b91d5de2e484f5bb0
Marak <marak.squires@gmail.com>
2025-06-03 02:08:23 -1000

    Scopes zalgo spell to window_container Removes background color for `pond` app allow for themes

--- v5/apps/based/spellbook/spells/zalgo/zalgo.js
+++ v5/apps/based/spellbook/spells/zalgo/zalgo.js
@@ -1,106 +1,102 @@
 // please no
-export default function castZalgoSpell (timeOut) {
-    console.log('tttt', timeOut)
-    if (timeOut === 0) {
-        timeOut = 9999;
-    }
-    $('span, label, a').each(function (i, item) {
+export default function castZalgoSpell(timeOut = 9999) {
+
+  $('span, label, a, h1, h2, h3, p', '.window-container').each(function (i, item) {
+    //if ($(this).css('display') === 'block') {
+    $(this).attr('data-og-zalgo', $(this).html());
+    $(this).html(_zalgo($(this).html()));
+    $(this).addClass('rainbowFast');
+    //}
+  });
+
+  setTimeout(function () {
+    $('span, label, a, h1, h2, h3, p', '.window-container').each(function (i, item) {
       //if ($(this).css('display') === 'block') {
-        $(this).attr('data-og-zalgo', $(this).html());
-        $(this).html(_zalgo($(this).html()));
-        $(this).addClass('rainbowFast');
+      $(this).html($(this).attr('data-og-zalgo'));
+      $(this).removeClass('rainbowFast');
       //}
     });
+  }, timeOut)
+
+};
+
+// he comes
+let _zalgo = function zalgo(text, options) {
+  text = text || '   he is here   ';
+  let soul = {
+    'up': [
+      '̍', '̎', '̄', '̅',
+      '̿', '̑', '̆', '̐',
+      '͒', '͗', '͑', '̇',
+      '̈', '̊', '͂', '̓',
+      '̈', '͊', '͋', '͌',
+      '̃', '̂', '̌', '͐',
+      '̀', '́', '̋', '̏',
+      '̒', '̓', '̔', '̽',
+      '̉', 'ͣ', 'ͤ', 'ͥ',
+      'ͦ', 'ͧ', 'ͨ', 'ͩ',
+      'ͪ', 'ͫ', 'ͬ', 'ͭ',
+      'ͮ', 'ͯ', '̾', '͛',
+      '͆', '̚',
+    ],
+    'down': [
+      '̖', '̗', '̘', '̙',
+      '̜', '̝', '̞', '̟',
+      '̠', '̤', '̥', '̦',
+      '̩', '̪', '̫', '̬',
+      '̭', '̮', '̯', '̰',
+      '̱', '̲', '̳', '̹',
+      '̺', '̻', '̼', 'ͅ',
+      '͇', '͈', '͉', '͍',
+      '͎', '͓', '͔', '͕',
+      '͖', '͙', '͚', '̣',
+    ],
+    'mid': [
+      '̕', '̛', '̀', '́',
+      '͘', '̡', '̢', '̧',
+      '̨', '̴', '̵', '̶',
+      '͜', '͝', '͞',
+      '͟', '͠', '͢', '̸',
+      '̷', '͡', ' ҉',
+    ],
+  };
+  let all = [].concat(soul.up, soul.down, soul.mid);
+
+  function randomNumber(range) {
+    let r = Math.floor(Math.random() * range);
+    return r;
+  }
+
+  function isChar(character) {
+    let bool = false;
+    all.filter(function (i) {
+      bool = (i === character);
+    });
+    return bool;
+  }
 
 
-    setTimeout(function(){
-        $('span, label, a').each(function (i, item) {
-            //if ($(this).css('display') === 'block') {
-              $(this).html($(this).attr('data-og-zalgo'));
-              $(this).removeClass('rainbowFast');
-            //}
-          });
-    }, timeOut)
-
-  }; 
-  
-  // he comes
-  let _zalgo = function zalgo (text, options) {
-    text = text || '   he is here   ';
-    let soul = {
-      'up': [
-        '̍', '̎', '̄', '̅',
-        '̿', '̑', '̆', '̐',
-        '͒', '͗', '͑', '̇',
-        '̈', '̊', '͂', '̓',
-        '̈', '͊', '͋', '͌',
-        '̃', '̂', '̌', '͐',
-        '̀', '́', '̋', '̏',
-        '̒', '̓', '̔', '̽',
-        '̉', 'ͣ', 'ͤ', 'ͥ',
-        'ͦ', 'ͧ', 'ͨ', 'ͩ',
-        'ͪ', 'ͫ', 'ͬ', 'ͭ',
-        'ͮ', 'ͯ', '̾', '͛',
-        '͆', '̚',
-      ],
-      'down': [
-        '̖', '̗', '̘', '̙',
-        '̜', '̝', '̞', '̟',
-        '̠', '̤', '̥', '̦',
-        '̩', '̪', '̫', '̬',
-        '̭', '̮', '̯', '̰',
-        '̱', '̲', '̳', '̹',
-        '̺', '̻', '̼', 'ͅ',
-        '͇', '͈', '͉', '͍',
-        '͎', '͓', '͔', '͕',
-        '͖', '͙', '͚', '̣',
-      ],
-      'mid': [
-        '̕', '̛', '̀', '́',
-        '͘', '̡', '̢', '̧',
-        '̨', '̴', '̵', '̶',
-        '͜', '͝', '͞',
-        '͟', '͠', '͢', '̸',
-        '̷', '͡', ' ҉',
-      ],
-    };
-    let all = [].concat(soul.up, soul.down, soul.mid);
-  
-    function randomNumber (range) {
-      let r = Math.floor(Math.random() * range);
-      return r;
-    }
-  
-    function isChar (character) {
-      let bool = false;
-      all.filter(function (i) {
-        bool = (i === character);
-      });
-      return bool;
-    }
-  
-  
-    function heComes (text, options) {
-      let result = '';
-      let counts;
-      let l;
-      options = options || {};
-      options['up'] =
-        typeof options['up'] !== 'undefined' ? options['up'] : true;
-      options['mid'] =
-        typeof options['mid'] !== 'undefined' ? options['mid'] : true;
-      options['down'] =
-        typeof options['down'] !== 'undefined' ? options['down'] : true;
-      options['size'] =
-        typeof options['size'] !== 'undefined' ? options['size'] : 'maxi';
-      text = text.split('');
-      for (l in text) {
-        if (isChar(l)) {
-          continue;
-        }
-        result = result + text[l];
-        counts = { 'up': 0, 'down': 0, 'mid': 0 };
-        switch (options.size) {
+  function heComes(text, options) {
+    let result = '';
+    let counts;
+    let l;
+    options = options || {};
+    options['up'] =
+      typeof options['up'] !== 'undefined' ? options['up'] : true;
+    options['mid'] =
+      typeof options['mid'] !== 'undefined' ? options['mid'] : true;
+    options['down'] =
+      typeof options['down'] !== 'undefined' ? options['down'] : true;
+    options['size'] =
+      typeof options['size'] !== 'undefined' ? options['size'] : 'maxi';
+    text = text.split('');
+    for (l in text) {
+      if (isChar(l)) {
+        continue;
+      }
+      result = result + text[l];
+      counts = { 'up': 0, 'down': 0, 'mid': 0 };
+      switch (options.size) {
         case 'mini':
           counts.up = randomNumber(4);
           counts.mid = randomNumber(2);
@@ -116,21 +112,20 @@ export default function castZalgoSpell (timeOut) {
           counts.mid = randomNumber(6) / 2;
           counts.down = randomNumber(8) + 1;
           break;
-        }
-  
-        let arr = [ 'up', 'mid', 'down' ];
-        for (let d in arr) {
-          let index = arr[d];
-          for (let i = 0; i <= counts[index]; i++) {
-            if (options[index]) {
-              result = result + soul[index][randomNumber(soul[index].length)];
-            }
+      }
+
+      let arr = ['up', 'mid', 'down'];
+      for (let d in arr) {
+        let index = arr[d];
+        for (let i = 0; i <= counts[index]; i++) {
+          if (options[index]) {
+            result = result + soul[index][randomNumber(soul[index].length)];
           }
         }
       }
-      return result;
     }
-    // don't summon him
-    return heComes(text, options);
-  };
-  
+    return result;
+  }
+  // don't summon him
+  return heComes(text, options);
+};
